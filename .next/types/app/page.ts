dcdd38b91495958593877a99a97bc62607fed2c4
Zulfiqar-Ali <zulfibalti592@gmail.com>
2026-04-19 13:12:22 +0500

    deployment portfolio

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/sadaqat/p/saqlain-portfolio/src/app/page.tsx
+// File: E:\2026\march\dr_saqlain_portfolio\Dr.SaqlainRaza\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
